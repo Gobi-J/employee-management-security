95a2b3bdf30a75d598653aa3545c74f3c15b8881
gobi.jagadeesh <2k20cse046@kiot.ac.in>
2024-10-08 23:00:19 +0530

    1:EmployeeManagementSecurity  - FIX: Made corrections in Variables, Methods segregation, Organizing imports, Class level elements

---
+++ src/main/java/com/i2i/ems/helper/ForbiddenException.java
@@ -0,0 +1,16 @@
+package com.i2i.ems.helper;
+
+
+public class ForbiddenException extends Exception {
+    public ForbiddenException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ForbiddenException(String message) {
+        super(message);
+    }
+
+    public ForbiddenException(Throwable cause) {
+        super(cause);
+    }
+}
